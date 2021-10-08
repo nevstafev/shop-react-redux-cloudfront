27f63f1e82dfc0bcbdb58a225357968fb67aee12
nevstafev <evstafev.n.e@gmail.com>
2021-10-08 12:26:07 +0300

    add import url (#3)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ql07tp6d73.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://p06pdmynpg.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
