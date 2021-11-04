962dadf7e7a15c9a63e3bfa7b26da7cc09fef673
nevstafev <evstafev.n.e@gmail.com>
2021-11-04 23:10:08 +0300

    add cart api url (#5)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ql07tp6d73.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://p06pdmynpg.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://53bk806jul.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
