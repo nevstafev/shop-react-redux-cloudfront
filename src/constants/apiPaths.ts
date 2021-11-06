93b8eb7208d1d4bf1d5de8e8a552f53051ffe824
nevstafev <evstafev.n.e@gmail.com>
2021-11-06 21:59:00 +0300

    use bff

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,7 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ql07tp6d73.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://p06pdmynpg.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://53bk806jul.execute-api.eu-west-1.amazonaws.com/dev/api',
+  bff: 'https://53bk806jul.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
